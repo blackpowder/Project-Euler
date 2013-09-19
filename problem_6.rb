f9cc2263c3023037b3bcf92fd85fbeee423c4b0c
Stephanie Noirpoudre <stephanie.noirpoudre@gmail.com>
2013-09-19 13:53:47 +0200

    problem 6 refactoring

--- problem_6.rb
+++ problem_6.rb
@@ -2,23 +2,21 @@ def sum_square_difference(low = 1, high = 100)
 	squares_of_numbers = []
 	numbers = []
 
-	difference = 0
-
 	(low..high).each do |number|
-		squares_of_number = number * number
-		squares_of_numbers << squares_of_number
-		sum_square_of_numbers = squares_of_numbers.inject {|sum, n| sum + n }
+		
+		squares_of_numbers << number * number
 
 		numbers << number
-		sum_of_numbers = numbers.inject {|sum, n| sum + n }
-		square_of_sum_of_numbers = sum_of_numbers * sum_of_numbers
-		difference = square_of_sum_of_numbers - sum_square_of_numbers
-	
 	end
-	
-	puts difference
+
+	sum_square_of_numbers = squares_of_numbers.inject {|sum, n| sum + n }
+
+	sum_of_numbers = numbers.inject {|sum, n| sum + n }
+	square_of_sum_of_numbers = sum_of_numbers * sum_of_numbers
+
+	square_of_sum_of_numbers - sum_square_of_numbers
 end
 
-sum_square_difference()
+puts sum_square_difference()
 
 
