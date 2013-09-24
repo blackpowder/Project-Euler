c63c56082a040c527e36aad25e213bd8063f3b39
Stephanie Noirpoudre <stephanie.noirpoudre@gmail.com>
2013-09-24 10:36:26 +0200

    refactoring problem 8

--- problem8.rb
+++ problem8.rb
@@ -1,41 +1,22 @@
-def numbers_serie
-	File.open("problem8.txt", "r") do |file|
-		file = file.read
-		@numbers_file = file.strip.split('')			
-	end
-	largest_product_in_serie
-end
-
 def largest_product_in_serie
 
-	n = 0
 	product_of_five_numbers = []
-
-	until n == 999 do 
-	
-		one = @numbers_file[0 + n].to_i
-		two = @numbers_file[1 + n].to_i
-		three = @numbers_file[2 + n].to_i
-		four = @numbers_file[3 + n].to_i
-		five = @numbers_file[4 + n].to_i
-
-		n += 1
-
-		product_of_five = one * two * three * four * five
-
+	read_file
+	(0..999).each do |n| 
+		product_of_five = 1
+		(0..4).each do |offset|
+			product_of_five = product_of_five * @numbers_file[n + offset].to_i
+		end
 		product_of_five_numbers << product_of_five		
 	end
-		product_of_five_numbers.max
+	product_of_five_numbers.max
 end 
-	
 
+def read_file
+	File.open("problem8.txt", "r") do |file|
+		file = file.read
+		@numbers_file = file.strip.split('')
+	end
+end
 
-puts numbers_serie
-
-
-	
-
-
-
-
-
+puts largest_product_in_serie
